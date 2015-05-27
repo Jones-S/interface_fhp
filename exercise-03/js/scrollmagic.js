1e712988def88315da23189f8fac0813b65e62ff
Jones <jonas.scheiwiller@zhdk.ch>
2015-05-27 15:21:31 +0200

    [update] first scrollmagic prototype for pre-release (draft)

--- exercise-03/js/scrollmagic.js
+++ exercise-03/js/scrollmagic.js
@@ -2,25 +2,37 @@
     $(function() { // Shorthand for $( document ).ready()
 
         // init controller
-        var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 0}, addIndicators: true});
+        // var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 0}, addIndicators: true});
+        var controller = new ScrollMagic.Controller();
 
         // build scenes
-        new ScrollMagic.Scene({triggerElement: "#sec1"})
-                        .setClassToggle("#high1", "active") // add class toggle
-                        .addIndicators() // add indicators (requires plugin)
+        // new ScrollMagic.Scene({triggerElement: "#sec1"})
+        //                 .setClassToggle("#high1", "active") // add class toggle
+        //                 .addIndicators() // add indicators (requires plugin)
+        //                 .addTo(controller);
+        // new ScrollMagic.Scene({triggerElement: "#sec2"})
+        //                 .setClassToggle("#high2", "active") // add class toggle
+        //                 .addIndicators() // add indicators (requires plugin)
+        //                 .addTo(controller);
+        // new ScrollMagic.Scene({triggerElement: "#sec3"})
+        //                 .setClassToggle("#high3", "active") // add class toggle
+        //                 .addIndicators() // add indicators (requires plugin)
+        //                 .addTo(controller);
+
+        // scroll scene pins svg for a certain time
+        var duration = 4000;
+        var duration2 = 2000;
+        new ScrollMagic.Scene({triggerElement: "#svg-trigger",triggerHook: 'onLeave', duration: duration})
+                        .setPin("#svg-pin")
+                        .addIndicators({name: "svg (duration: " + duration + ")"}) // add indicators (requires plugin)
                         .addTo(controller);
-        new ScrollMagic.Scene({triggerElement: "#sec2"})
-                        .setClassToggle("#high2", "active") // add class toggle
-                        .addIndicators() // add indicators (requires plugin)
+
+        // scroll scene adds a trigger for displaying svg groups
+        new ScrollMagic.Scene({triggerElement: "#svg-trigger", triggerHook: 'onLeave', duration: duration2, offset: 2000})
+                        .setClassToggle("#red_marks circle", "display") // add class toggle
+                        .addIndicators({name: "red marks (duration: " + duration2 + ")"}) // add indicators (requires plugin)
                         .addTo(controller);
-        new ScrollMagic.Scene({triggerElement: "#sec3"})
-                        .setClassToggle("#high3", "active") // add class toggle
-                        .addIndicators() // add indicators (requires plugin)
-                        .addTo(controller);
-        new ScrollMagic.Scene({triggerElement: "#sec4"})
-                        .setClassToggle("#high4", "active") // add class toggle
-                        .addIndicators() // add indicators (requires plugin)
-                        .addTo(controller);
+
 
     });
 }(jQuery));
